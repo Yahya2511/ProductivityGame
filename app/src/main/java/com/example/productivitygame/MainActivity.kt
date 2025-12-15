bfc2261d64380079781884fe3a1a4f422369a047
AbdoSouls <abdobakrfayed@gmail.com>
2025-12-15 02:05:31 +0200

    cleaned

--- app/src/main/java/com/example/productivitygame/MainActivity.kt
+++ app/src/main/java/com/example/productivitygame/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.content.Intent
 
+//main
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
